2266ad4b0404ea1190fcbe086761d4ad56dc567b
Waplll <maximdmitriys@gmail.com>
2025-03-31 10:45:30 +0700

    add registration

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,7 @@ namespace Controller;
 use Model\Post;
 use Src\View;
 use Src\Request;
+use Model\User;
 
 class Site
 {
@@ -17,5 +18,12 @@ class Site
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
+    }
+    public function signup(Request $request): string
+    {
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/go');
+        }
+        return new View('site.signup');
     }
 }
